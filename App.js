42257aca97613532f2392f68b7cf3bd6cc23425a
VanessaVinodMaliakal <64256928+VanessaVinodMaliakal@users.noreply.github.com>
2021-01-29 18:59:59 +0530

    Add files via upload
    Making use of API call and services and thereby creating an online Dictionary app in Visual Studio Code.

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen.js';
+//import AppHeader from './header.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <HomeScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
